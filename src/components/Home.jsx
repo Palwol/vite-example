acf87bc2d90a9e39bdd72b84eba116e7b639accd
palwol <mirim920@naver.com>
2024-05-15 14:52:47 +0900

    feat: Home 페이지 배너 및 네비게이션 UI 추가

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,28 @@
+import {navigationList} from '@/static/navigationList';
+import styles from '@/styles/Home.module.css';
+import {Link} from 'react-router-dom';
+
 const Home = () => {
-  return <main></main>;
+  return (
+    <>
+      <header>
+        <div className={styles.banner}>{`DJMax\nStore`}</div>
+        <nav>
+          <ul>
+            {navigationList.map((nav) => (
+              <Link key={nav.name} to={nav.url}>
+                <li>
+                  <img src={nav.img} width={25} height={25} />
+                  {nav.name}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <main></main>
+    </>
+  );
 };
 
 export default Home;
