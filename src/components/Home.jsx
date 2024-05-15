eeecc8c52337497aac9ee8f275a2ed6b68131d07
palwol <mirim920@naver.com>
2024-05-15 15:14:16 +0900

    feat: 홈페이지 x, plus 아이콘 가져오는 방식 수정

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
+import {PlusIcon, XIcon} from '@/assets';
 import {navigationList} from '@/static/navigationList';
 import styles from '@/styles/Home.module.css';
+import {isOdd} from '@/utils/isOdd';
 import {Link} from 'react-router-dom';
 
 const Home = () => {
@@ -9,10 +11,14 @@ const Home = () => {
         <div className={styles.banner}>{`DJMax\nStore`}</div>
         <nav>
           <ul>
-            {navigationList.map((nav) => (
+            {navigationList.map((nav, index) => (
               <Link key={nav.name} to={nav.url}>
                 <li>
-                  <img src={nav.img} width={25} height={25} />
+                  {isOdd(index) ? (
+                    <PlusIcon width={25} height={25} />
+                  ) : (
+                    <XIcon width={25} height={25} />
+                  )}
                   {nav.name}
                 </li>
               </Link>
--- src/static/navigationList.js
+++ src/static/navigationList.js
@@ -1,30 +1,22 @@
-import plugIcon from '@/assets/plus.svg';
-import xIcon from '@/assets/xmark.svg';
-
 export const navigationList = [
   {
     name: 'home',
-    img: xIcon,
     url: '/',
   },
   {
     name: 'notice',
-    img: plugIcon,
     url: 'notice',
   },
   {
     name: 'my page',
-    img: xIcon,
     url: 'mypage',
   },
   {
     name: 'login',
-    img: plugIcon,
     url: 'login',
   },
   {
     name: 'cart',
-    img: xIcon,
     url: 'cart',
   },
 ];
